8bccf53c30f3f962162554c0001851b465a00d96
MarzukaaZaki <zakimarzuka@gmail.com>
2023-10-03 20:19:09 +0600

    Set up tab logic

--- src/components/PropertiesByCity/PropertiesByCity.jsx
+++ src/components/PropertiesByCity/PropertiesByCity.jsx
@@ -8,25 +8,38 @@ const PropertiesByCity = () => {
     // console.log(allListedProperties);
 
     // Set Active Tab Logic
-    const [ activeTab, setActiveTab ] = useState('');
+    const [activeTab, setActiveTab] = useState('New York'); // By default, show the properties of first tab New York
     const handleTabClick = (cityName) => {
         setActiveTab(cityName)
     }
+
+    // Set Show More button logic
+
+    console.log(activeTab);
+
+    const tabPropertyData = allListedProperties[activeTab];
+    console.log(tabPropertyData); 
+
 
 
 
     return (
         <div>
-            <SectionTitle 
-            heading='Featured Listed Property' 
-            subHeading='Real estate can be bought, sold, leased or rented, and can be a valuable investment opportunity. The value of a real estate can be...'/>
+            <SectionTitle
+                heading='Featured Listed Property'
+                subHeading='Real estate can be bought, sold, leased or rented, and can be a valuable investment opportunity. The value of a real estate can be...' />
 
             {/* Tabs */}
-            <div className='flex justify-start gap-5'>
-            <div onClick={() => handleTabClick("New York")} className={`rounded-full   font-semibold px-5 py-3 shadow-md ${activeTab == "New York" ? "bg-[#373ae3] text-white" : "bg-[#ebebfb]"}`}>New York</div>
-            <div onClick={() => handleTabClick("Mumbai")} className= "rounded-full font-semibold  px-6 py-3 shadow-md bg-[#ebebfb]">Mumbai</div>
-            <div onClick={() => handleTabClick("Paris")} className= "rounded-full font-semibold  px-6 py-3 shadow-md bg-[#ebebfb]">Paris </div>
-            <div onClick={() => handleTabClick("London")} className= "rounded-full font-semibold  px-6 py-3 shadow-md bg-[#ebebfb]">London</div>
+            <div className='flex justify-start gap-5 mb-5'>
+                <div onClick={() => handleTabClick("New York")} className={`rounded-full   font-semibold px-5 py-3  ${activeTab == "New York" ? "bg-[#373ae3] text-white" : "bg-[#ebebfb]"}`}>New York</div>
+                <div onClick={() => handleTabClick("Mumbai")} className={`rounded-full   font-semibold px-5 py-3  ${activeTab == "Mumbai" ? "bg-[#373ae3] text-white" : "bg-[#ebebfb]"}`}>Mumbai</div>
+                <div onClick={() => handleTabClick("Paris")} className={`rounded-full   font-semibold px-5 py-3  ${activeTab == "Paris" ? "bg-[#373ae3] text-white" : "bg-[#ebebfb]"}`}>Paris </div>
+                <div onClick={() => handleTabClick("London")} className={`rounded-full   font-semibold px-5 py-3  ${activeTab == "London" ? "bg-[#373ae3] text-white" : "bg-[#ebebfb]"}`}>London</div>
+            </div>
+            <div className='grid md:grid-cols-3'>
+                {
+                    tabPropertyData.map(property => <p property={property}>{property.popularity}</p>)
+                }
             </div>
 
         </div>
